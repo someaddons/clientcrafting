df6d78f1866795dcfb40419dabccb9cbba933e9e
someaddons <someaddons@github.com>
2023-08-18 20:25:14 +0200

    1.7: Cleanup

--- src/main/java/com/clientcrafting/ClientCraftingMod.java
+++ src/main/java/com/clientcrafting/ClientCraftingMod.java
@@ -1,6 +1,5 @@
 package com.clientcrafting;
 
-import com.clientcrafting.config.Configuration;
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.resources.ResourceLocation;
 import org.apache.logging.log4j.LogManager;
@@ -12,20 +11,10 @@ public class ClientCraftingMod implements ModInitializer {
 
     public static final String MOD_ID = "clientcrafting";
     public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
-    private static Configuration config = null;
     public static Random rand = new Random();
 
     @Override
     public void onInitialize() {
-    }
-
-    public static Configuration getConfig() {
-        if (config == null) {
-            config = new Configuration();
-            config.load();
-        }
-
-        return config;
     }
 
     public static ResourceLocation id(String name) {

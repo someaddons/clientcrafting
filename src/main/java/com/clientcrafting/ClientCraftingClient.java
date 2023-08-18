0bd996661aa4a1a1419e486047e5d8bb79054942
someaddons <someaddons@github.com>
2023-08-18 20:32:58 +0200

    1.7: cleanup

--- src/main/java/com/clientcrafting/ClientCraftingClient.java
+++ src/main/java/com/clientcrafting/ClientCraftingClient.java
@@ -1,10 +1,6 @@
 package com.clientcrafting;
 
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-
 public class ClientCraftingClient
 {
-    public static void onInitializeClient(final FMLClientSetupEvent event)
-    {
-    }
+
 }
--- src/main/java/com/clientcrafting/ClientCraftingMod.java
+++ src/main/java/com/clientcrafting/ClientCraftingMod.java
@@ -1,10 +1,6 @@
 package com.clientcrafting;
 
-import com.clientcrafting.config.Configuration;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -16,34 +12,11 @@ import static com.clientcrafting.ClientCraftingMod.MOD_ID;
 @Mod(MOD_ID)
 public class ClientCraftingMod
 {
-    public static final String        MOD_ID = "clientcrafting";
-    public static final Logger        LOGGER = LogManager.getLogger();
-    private static      Configuration config = null;
-    public static       Random        rand   = new Random();
+    public static final String MOD_ID = "clientcrafting";
+    public static final Logger LOGGER = LogManager.getLogger();
+    public static       Random rand   = new Random();
 
     public ClientCraftingMod()
     {
-    }
-
-    @SubscribeEvent
-    public void clientSetup(FMLClientSetupEvent event)
-    {
-        // Side safe client event handler
-    }
-
-    private void setup(final FMLCommonSetupEvent event)
-    {
-        LOGGER.info(MOD_ID + " mod initialized");
-    }
-
-    public static Configuration getConfig()
-    {
-        if (config == null)
-        {
-            config = new Configuration();
-            config.load();
-        }
-
-        return config;
     }
 }
